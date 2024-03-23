a7d5872e55c3795dea2b631aee4c2de3ddd7b1ae
patrickklapid@gmail.com <patrickklapid@gmail.com>
2024-03-23 15:02:10 -0400

    file upload component + styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { Text } from "@mantine/core";
+import { Card, Group, Stack, Text } from "@mantine/core";
+import FileDropZone from "./components/FileDropzone";
 
 function App() {
   return (
-    <div className="App">
-      <Text ta="center">Upload File</Text>
+    <div>
+      <Stack mt={80} align="center" gap="lg">
+        <Text ta="center" fw={700} size="xl">
+          Upload Document for Text Extraction
+        </Text>
+        <Card bg="#fbfbff" radius="md" padding="lg" shadow="md">
+          <Group>
+            <FileDropZone />
+            <div style={{ width: 500 }}>
+              <Text>Text Generation</Text>
+            </div>
+          </Group>
+        </Card>
+      </Stack>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "@mantine/dropzone/styles.css";
+import "@mantine/core/styles.css";
 import App from "./App";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, createTheme } from "@mantine/core";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const theme = createTheme({
+  fontFamily: "syncSwap, sans-serif",
+  other: { bodyBackground: "#efeefe" },
+});
+
 root.render(
   <React.StrictMode>
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <App />
     </MantineProvider>
   </React.StrictMode>
