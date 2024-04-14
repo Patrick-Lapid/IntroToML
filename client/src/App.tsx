cb8ddb14aef9e73c56c36e4776829b744a4c879c
patrickklapid@gmail.com <patrickklapid@gmail.com>
2024-04-14 03:09:53 -0400

    style: added gradient mesh background animation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,28 @@
-import { Card, Group, Stack, Text } from "@mantine/core";
+import { Card, Group, Stack, Text, rgba } from "@mantine/core";
 import Typewriter from "typewriter-effect";
 import FileDropZone from "./components/FileDropzone";
 
 function App() {
   return (
-    <div>
-      <Stack mt={80} align="center" gap="lg">
+    <>
+      <div className="absolute">
+        <div className="meshContainer">
+          <div className="bg-shape1 bg-primary bg-blur"></div>
+          <div className="bg-shape2 bg-purple bg-blur"></div>
+          <div className="bg-shape3 bg-teal bg-blur"></div>
+        </div>
+      </div>
+      <Stack mt={120} align="center" gap="lg">
         <Text ta="center" fw={700} size="xl">
           Upload Document for Text Extraction
         </Text>
-        <Card bg="#fbfbff" radius="md" padding="lg" shadow="md">
+        <Card
+          // bg="#fbfbff"
+          radius="md"
+          padding="lg"
+          shadow="md"
+          bg={rgba("#FFFFFF", 0.6)}
+        >
           <Group justify="space-between">
             <FileDropZone />
             <div style={{ width: 500 }}>
@@ -24,7 +37,7 @@ function App() {
           </Group>
         </Card>
       </Stack>
-    </div>
+    </>
   );
 }
 
