98ba7574e953e41a08f056d05a1d248dd28a639e
patrickklapid@gmail.com <patrickklapid@gmail.com>
2024-04-20 09:01:43 -0400

    build: configured Application-Type for Api call

--- server/main.py
+++ server/main.py
@@ -29,7 +29,7 @@ def read_imagefile(file) -> Image.Image:
 @app.post("/predict-image/")
 async def predict_image(file: UploadFile = File(...)):
     time.sleep(4)
-    return "Test"
+    return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     if file.content_type.startswith('image/'):
         image = read_imagefile(await file.read())
         # Assuming model expects numpy array
