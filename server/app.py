b31d00b7d3fefaf65f7bd6a0d90ac655539bf6be
patrickklapid@gmail.com <patrickklapid@gmail.com>
2024-04-20 08:11:31 -0400

    feat: configured CORS for localhost frontend

--- server/app.py
+++ server/app.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import numpy as np
 from io import BytesIO
+import time
 from PIL import Image
 import pickle
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 
 # Loads ML model
 # with open('model.pkl', 'rb') as model_file:
@@ -17,6 +28,8 @@ def read_imagefile(file) -> Image.Image:
 
 @app.post("/predict-image/")
 async def predict_image(file: UploadFile = File(...)):
+    time.sleep(4)
+    return "Test"
     if file.content_type.startswith('image/'):
         image = read_imagefile(await file.read())
         # Assuming model expects numpy array
